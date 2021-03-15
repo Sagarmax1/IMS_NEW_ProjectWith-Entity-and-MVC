7b7a2acd1aa92362b488b28d7db5a7a250e7a011
Sagarmax1 <sagar.srivastava@gmail.com>
2021-03-15 20:19:06 +0530

    code upload

---
+++ IMS_NEW/Controllers/AdminController.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using IMS_NEW.Models;
+using System.IO;
+
+namespace IMS_NEW.Controllers
+{
+    public class AdminController : Controller
+    {
+        IMSEntities Ims = new IMSEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)   // This Syntax For Restriction to go without login on AssetMaster Page
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            return View(Ims.tbl_UserRegistration.ToList());
+        }
+
+
+        // Export to excel code 
+
+
+
+        // Export to excel
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tbl_UserRegistration user)
+        {
+            try
+            {
+                if (ModelState.IsValid == true)
+                {
+                    //####### 1st Method for Insert Record In Database without extra column value #######//
+
+                    //Ims.tbl_UserRegistration.Add(user); 
+                    //Ims.SaveChanges();                   
+                    //ViewBag.Message = "Insert SuccessFull";
+
+                    // ################################################################# //
+
+                    //##### 2nd Method for Insert Record In database with Extra Column Value ######//
+
+                    tbl_UserRegistration add = new tbl_UserRegistration();
+                    add.UserName = user.UserName;
+                    add.Email = user.Email;
+                    add.Password = user.Password;
+                    add.CreatedOn = DateTime.Now;
+                    Ims.tbl_UserRegistration.Add(add);
+                    Ims.SaveChanges();
+                    ViewBag.Message = "Insert SuccessFull Go to Login Page";
+
+                    //###########################################################//
+
+                }
+                ModelState.Clear();
+                // return RedirectToAction("Login", "Admin");
+                return View();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(tbl_UserRegistration user)
+        {
+            try
+            {
+                if (ModelState.IsValid == true)
+                {
+                    var data = Ims.tbl_UserRegistration.Where(a => a.UserName == user.UserName && a.Password == user.Password).FirstOrDefault();
+                    if (data == null)
+                    {
+                        ViewBag.ErrorMessage = "Login Fail Please Enter Correct Information !";
+                        return View();
+                    }
+                    else
+                    {
+                        Session["UserName"] = user.UserName;
+                        // Session["UserId"] = user.Id;
+                        return RedirectToAction("AssetMaster", "Asset");
+                    }
+                }
+                ModelState.Clear();
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Admin");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var row = Ims.tbl_UserRegistration.Where(a => a.Id == id).FirstOrDefault();
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tbl_UserRegistration user)
+        {
+            if (ModelState.IsValid == true)
+            {
+                Ims.Entry(user).State = EntityState.Modified;
+                //tbl_UserRegistration add = new tbl_UserRegistration();
+                //add.UserName = user.UserName;
+                //add.Email = user.Email;
+                //add.Password = user.Password;
+                //add.CreatedOn = DateTime.Now;
+                // Ims.tbl_UserRegistration.Add(user);
+                int a = Ims.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["UpdateMesage"] = " <script>alert('User Update SuccessFull')</Script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["UpdateMessage"] = " <script>alert('User Not Update')</Script>";
+                }
+            }
+            return View("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (id > 0)
+            {
+                var Delete = Ims.tbl_UserRegistration.Where(a => a.Id == id).FirstOrDefault();
+                if (Delete != null)
+                {
+                    Ims.Entry(Delete).State = EntityState.Deleted;
+                    int a = Ims.SaveChanges();
+                    if (a > 0)
+                    {
+                        TempData["DeleteMesage"] = " <script>alert('Delete SuccessFull')</Script>";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        TempData["DeleteMesage"] = " <script>alert('Not Deleted')</Script>";
+                    }
+                }
+
+            }
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("FileUpload")]
+        public JsonResult FileUpload_Post()
+        {
+            bool flag = true;
+            string responseMessage = string.Empty;
+
+            if (Request.Files.Count > 0)
+            {
+                HttpPostedFileBase file = Request.Files[0];
+
+                
+                if (file != null && file.ContentLength > 0 && (Path.GetExtension(file.FileName).ToLower() == ".xlsx" || Path.GetExtension(file.FileName).ToLower() == ".xls"))
+                {
+                    try
+                    {
+                        string fileName = Path.GetFileName(file.FileName);
+                        string filePath = Path.Combine(Server.MapPath("~/UploadFiles"), fileName);
+                        file.SaveAs(filePath);
+
+                        flag = true;
+                        responseMessage = "Upload Successful.";
+                    }
+                    catch (Exception ex)
+                    {
+                        flag = false;
+                        responseMessage = "Upload Failed with error: " + ex.Message;
+                    }
+                }
+                else
+                {
+                    flag = false;
+                    responseMessage = "File is invalid.";
+                }
+            }
+            else
+            {
+                flag = false;
+                responseMessage = "File Upload has no file.";
+            }
+
+
+            return Json(new { success = flag, responseMessage = responseMessage }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
---
+++ IMS_NEW/Controllers/AssetController.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using IMS_NEW.Models;
+
+namespace IMS_NEW.Controllers
+{
+    public class AssetController : Controller
+    {
+        IMSEntities Ims = new IMSEntities();
+        // GET: Asset
+        public ActionResult AssetMasterList()
+        {
+            return View(Ims.tbl_AssetMaster.ToList());
+        }
+
+        public ActionResult Edit(int AssetId)
+        {
+            var row = Ims.tbl_AssetMaster.Where(a => a.AssetId == AssetId).FirstOrDefault();
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tbl_AssetMaster user)
+        {
+            if (ModelState.IsValid == true)
+            {
+                //Ims.Entry(user).State = EntityState.Modified;
+                //int a = Ims.SaveChanges();
+                //if (a > 0)
+                //{
+                //    TempData["UpdateMesage"] = " <script>alert('Asset Update SuccessFull')</Script>";
+                //    return RedirectToAction("AssetMasterList");
+                //}
+                //else
+                //{
+                //    TempData["UpdateMessage"] = " <script>alert('Asset Not Update')</Script>";
+                //}
+
+
+
+                using (IMSEntities obj = new IMSEntities())
+                {
+                    tbl_AssetMaster asset = (from m in obj.tbl_AssetMaster
+                                             where m.AssetId == user.AssetId
+                                             select m).FirstOrDefault();
+                  
+                    asset.UserName = user.UserName;
+                    asset.LocationId = user.LocationId;
+                    asset.Ram = user.Ram;
+                    asset.WarrantyStatus = user.WarrantyStatus;
+                    asset.WindowKey = user.WindowKey;
+
+                    int e = obj.SaveChanges();
+                    if (e > 0)
+                    {
+                        TempData["UpdateMesage"] = " <script>alert('Asset Update SuccessFull')</Script>";
+                        return RedirectToAction("AssetMasterList");
+                    }
+                    else
+                    {
+                        TempData["UpdateMessage"] = " <script>alert('Asset Not Update')</Script>";
+                    }
+
+                }
+
+
+            }
+            //  return View("AssetMasterList");
+                return RedirectToAction("AssetMasterList");
+
+        }
+
+
+        public ActionResult Delete(int AssetId)
+        {
+            if (AssetId > 0)
+            {
+                var Delete = Ims.tbl_AssetMaster.Where(a => a.AssetId == AssetId).FirstOrDefault();
+                if (Delete != null)
+                {
+                    Ims.Entry(Delete).State = EntityState.Deleted;
+                   
+                    int a = Ims.SaveChanges();
+                    if (a > 0)
+                    {
+                        TempData["DeleteMesage"] = " <script>alert('Are You Sure You Want To Delete !')</Script>";
+                       
+                        return RedirectToAction("AssetMasterList");
+                    }
+                    else
+                    {
+                        TempData["DeleteMesage"] = " <script>alert('Not Deleted')</Script>";
+                    }
+                }
+
+            }
+            return View("AssetMasterList");
+        }
+
+
+
+        //[HttpPost]
+        // public ActionResult Clear()
+        // {
+        //     return View("AssetMaster");
+        // }
+
+        [HttpGet]
+        public ActionResult AssetMaster()
+        {
+            if (Session["UserName"] == null)   // This Syntax For Restriction to go without login on AssetMaster Page
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+
+            tbl_AssetMaster asset = new tbl_AssetMaster();
+            using (IMSEntities db = new IMSEntities())
+            {
+                // asset.AssetTypeCollection = db.tbl_AssetType.ToList<tbl_AssetType>();
+            }
+            // ViewBag.AssetTypeId = new SelectList(Ims.tbl_AssetType, "AssetTypeId", "AssetTypeName");
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AssetMaster(tbl_AssetMaster user)
+        {
+            try
+            {
+                if (ModelState.IsValid == true)
+                {
+
+                    tbl_AssetMaster add = new tbl_AssetMaster();
+                    //add.AssetTypeId = user.AssetTypeId.Value;
+                   // add.LocationId = user.LocationId.Value;
+                     add.AssetTypeId = 1;
+                     add.LocationId = 1;
+                    add.UserName = user.UserName;
+                    add.SerialNo = user.SerialNo;
+                    //  add.AssetsCode = user.AssetsCode;
+                    add.Ram = user.Ram;
+                    add.PurchaseDate = user.PurchaseDate;
+                    add.WarrantyStatus = user.WarrantyStatus;
+                    add.WindowKey = user.WindowKey;
+                    add.Model = user.Model.ToString();
+                    // add.CreatedBy = Convert.ToInt32(Session["UserId"].ToString());
+                    add.CreatedBy = 1;
+
+                    add.CreatedOn = DateTime.Now;
+                    // add.UpdatedBy = Convert.ToInt32(Session["UserId"].ToString());
+                    add.UpdatedBy = 1;
+                    add.UpdatedOn = DateTime.Now;
+                    Ims.tbl_AssetMaster.Add(add);
+                    Ims.SaveChanges();
+                    ViewBag.Message = "Asset Added Successfully";
+                }
+                ModelState.Clear();
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AssetTransaction()
+        {
+            if (Session["UserName"] == null)   // This Syntax For Restriction to go without login on AssetMaster Page
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AssetTransaction(tbl_AssetUserMaping user)
+        {
+            try
+            {
+                if (ModelState.IsValid == true)
+                {
+                    tbl_AssetUserMaping add = new tbl_AssetUserMaping();
+                    add.AssetsCode = user.AssetsCode;
+                    add.AssetId = user.AssetId.Value;
+                    add.UserLocationid = user.UserLocationid;
+                    // add.AssetId = 1;
+                    add.UserName = user.UserName;
+                    add.Emailid = user.Emailid;
+                   
+                    add.AssignDate = user.AssignDate;
+                    add.TransferDate = user.TransferDate;
+                    add.AssetLocationId = user.AssetLocationId;
+                    // add.CreatedBy = Convert.ToInt32(Session["UserId"].ToString());
+                    // add.CreatedBy = 1;
+                    add.CreatedBy = user.CreatedBy;
+                    add.CreatedOn = DateTime.Now;
+                    // add.UpdatedBy = Convert.ToInt32(Session["UserId"].ToString());
+                    add.UpdatedBy = 1;
+                    add.UpdatedOn = DateTime.Now;
+                    Ims.tbl_AssetUserMaping.Add(add);
+                    Ims.SaveChanges();
+                    ViewBag.Message = "Asset Set For User Successfully";
+
+                }
+                ModelState.Clear();
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public ActionResult AssetTransactionList()
+        {
+            return View(Ims.tbl_AssetUserMaping.ToList());
+        }
+
+
+
+        //new Coding For Edit Case//
+    
+        //    [HttpGet]
+        //  public ActionResult AssetTrEdit(int id)
+        //{
+        //    using (IMSEntities obj = new IMSEntities())
+        //    {
+        //        tbl_AssetUserMaping asset = (from m in obj.tbl_AssetUserMaping
+        //                                     where m.AssetUserMapplingid == id
+        //                                     select m).FirstOrDefault();
+
+        //        return View(asset);
+        //    }
+        //}
+
+        //[HttpPost]
+        //public ActionResult AssetTrEdit(tbl_AssetUserMaping objasset)
+        //{
+        //    using (IMSEntities obj = new IMSEntities())
+        //    {
+        //        tbl_AssetUserMaping asset = (from m in obj.tbl_AssetUserMaping
+        //                                     where m.AssetUserMapplingid == objasset.AssetUserMapplingid
+        //                                     select m).FirstOrDefault();
+        //        asset.AssetsCode = objasset.AssetsCode;
+        //        asset.UserName = objasset.UserName;
+        //        obj.SaveChanges();
+                
+        //    }
+        //    return RedirectToAction("AssetTransactionList");
+        //}
+
+
+
+        //new Coding For Edit Case//
+
+        public ActionResult EdtForAsTranscationList(int AssetUserMapplingid)
+        {
+            var row = Ims.tbl_AssetUserMaping.Where(a => a.AssetUserMapplingid == AssetUserMapplingid).FirstOrDefault();
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult EdtForAsTranscationList(tbl_AssetUserMaping user)
+        {
+            if (ModelState.IsValid == true)
+            {
+                Ims.Entry(user).State = EntityState.Modified;
+                int a = Ims.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["UpdateMesage"] = " <script>alert('Asset Update SuccessFull')</Script>";
+                    return RedirectToAction("AssetTransactionList");
+                }
+                else
+                {
+                    TempData["UpdateMessage"] = " <script>alert('Asset Not Update')</Script>";
+                }
+            }
+            return View("AssetTransactionList");
+        }
+
+        public ActionResult DLTForAsTranscationList(int AssetUserMapplingid)
+        {
+            if (AssetUserMapplingid > 0)
+            {
+                var Delete = Ims.tbl_AssetUserMaping.Where(a => a.AssetUserMapplingid == AssetUserMapplingid).FirstOrDefault();
+                if (Delete != null)
+                {
+                    Ims.Entry(Delete).State = EntityState.Deleted;
+
+                    int a = Ims.SaveChanges();
+                    if (a > 0)
+                    {
+                        TempData["DeleteMesage"] = " <script>alert('Are You Sure You Want To Delete !')</Script>";
+
+                        return RedirectToAction("AssetTransactionList");
+                    }
+                    else
+                    {
+                        TempData["DeleteMesage"] = " <script>alert('Not Deleted')</Script>";
+                    }
+                }
+
+            }
+            return View("AssetTransactionList");
+        }
+
+    }
+}
